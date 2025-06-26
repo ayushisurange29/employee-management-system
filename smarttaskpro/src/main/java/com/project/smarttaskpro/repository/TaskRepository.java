5f1cff865a74f6887c757d64f4cba2bc858d21d1
ayushisurange29 <ayushisurange29@gmail.com>
2025-06-26 14:09:14 +0530

    Delete smarttaskpro directory

--- smarttaskpro/src/main/java/com/project/smarttaskpro/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.smarttaskpro.repository;
-
-import com.project.smarttaskpro.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task,Long> {
-}
